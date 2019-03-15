90f660f19105b8326b84ede0b121f157f9891364
Vishwas RN <vreddynaga@hawk.iit.edu>
2019-03-15 17:54:11 -0500

    Create Register.php

---
+++ Register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Reservation</title>
+<meta name="reservation hotel for malaysia" >
+<meta name="zulkarnain" content="gambohnetwork.com.my">
+<meta name="copyright" content="Hotel Malaysia, inc. Copyright (c) 2014">
+<link rel="stylesheet" href="scss/foundation.css">
+<link rel="stylesheet" href="scss/style.css">
+<link href='http://fonts.googleapis.com/css?family=Slabo+13px' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="build/css/intlTelInput.css">
+<!--link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="icon/css/fontello.css">
+<link rel="stylesheet" href="icon/css/animation.css"><!--[if IE 7]><link rel="stylesheet" href="css/fontello-ie7.css"><![endif]>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script-->
+<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+
+<meta class="foundation-data-attribute-namespace"><meta class="foundation-mq-xxlarge"><meta class="foundation-mq-xlarge"><meta class="foundation-mq-large"><meta class="foundation-mq-medium"><meta class="foundation-mq-small"><style></style><meta class="foundation-mq-topbar"></head>
+<body class="fontbody">
+<div class="row foo" style="margin:30px auto 30px auto;">
+
+
+</div>
+</div>
+
+<div class="row">
