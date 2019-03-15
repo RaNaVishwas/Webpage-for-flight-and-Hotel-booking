3e6a3c2376f3d2f566d9e113dba842f2b8c90932
Vishwas RN <vreddynaga@hawk.iit.edu>
2019-03-15 17:24:11 -0500

    Update ReservationDetail.php

--- ReservationDetail.php
+++ ReservationDetail.php
@@ -55,3 +55,55 @@ $_SESSION["totalchild"] = $_POST['totalchild'];
 				</p>
 			</div>
 		  </div>
+
+
+		  <p><b>Room Details</b></p>
+		<div class="row">
+			<div class="large-6 columns">
+				<p>Check In Date :<b><?php echo $_SESSION["checkindate"];?></b></p>
+			</div>
+			<div class="large-6 columns">
+			  <p>Check Out Date :<b><?php echo $_SESSION["checkoutdate"];?></b></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="large-6 columns">
+			  <p>Adult :<b><?php echo $_SESSION["totaladult"];?></b></p>
+			</div>
+			<div class="large-6 columns">
+			  <p>Children :<b><?php echo $_SESSION["totalchild"];?></b></p>
+			</div>
+		</div> 
+		  
+		  <div class="row">
+			<div class="large-12 columns" >
+
+			</div>
+		  </div>
+		</form>
+		
+			<div class="row">
+				<br>
+				<div class="large-12 columns" >
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" >
+					<input type="hidden" name="cmd" value="_xclick">
+					<input type="hidden" name="business" value="mrzulkarnine@gmail.com">
+					<input type="hidden" name="lc" value="MY">
+					<input type="hidden" name="item_name" value="15% Hotel Deposit Payment">
+					<input type="hidden" name="amount" value="<?php $amount = $_POST['global_deposit']; print $amount; ?>">
+					<input type="hidden" name="currency_code" value="MYR">
+					<input type="hidden" name="button_subtype" value="services">
+					<input type="hidden" name="no_note" value="0">
+					<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest">
+					<input type="image" src="img/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" style="width:20%; height:20%; background-color:#2ecc71; padding:1%;">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+					</form>
+				</div>
+			</div>
+
+	</div>
+
+
+</div>
+
+</body></html>
