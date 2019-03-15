ca3fa4920b10697fed703569189e500093dbec0a
Vishwas RN <vreddynaga@hawk.iit.edu>
2019-03-15 17:48:11 -0500

    Update ReservationComplete.php

--- ReservationComplete.php
+++ ReservationComplete.php
@@ -216,3 +216,13 @@ if(mysqli_num_rows($re) > 0)
 				  </div>
 				</form>
 		</div>
+
+		</div>
+	<div class="large-8 columns blackblur fontcolor" style="padding:10px">
+	
+		<div class="large-12 columns" >
+		<p><b>Reservation Complete</b></p><hr class="line">
+		<p>Details of your reservation have just been sent to you
+		in a confirmation email. Please check your spam folder if you didn't received any email. We look forward to see you soon. In the
+		meantime, if you have any questions, feel free to contact us.</p>
+		<p>
