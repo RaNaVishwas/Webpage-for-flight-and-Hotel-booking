c82112e8a7b84861f93c03ff948f3d2d2c5f10ed
Vishwas RN <vreddynaga@hawk.iit.edu>
2019-03-15 17:22:16 -0500

    Create RoomSelection.php

---
+++ RoomSelection.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Reservation</title>
+<meta name="reservation hotel for malaysia" >
+<meta name="zulkarnain" content="gambohnetwork.com.my">
+<meta name="copyright" content="Hotel Malaysia, inc. Copyright (c) 2014">
+<link rel="stylesheet" href="scss/foundation.css">
+<link rel="stylesheet" href="scss/style.css">
+<link href='http://fonts.googleapis.com/css?family=Slabo+13px' rel='stylesheet' type='text/css'>
+<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+<script src="jquery.backstretch.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+<meta class="foundation-data-attribute-namespace"><meta class="foundation-mq-xxlarge"><meta class="foundation-mq-xlarge"><meta class="foundation-mq-large"><meta class="foundation-mq-medium"><meta class="foundation-mq-small"><style></style><meta class="foundation-mq-topbar"></head>
+<body class="fontbody">
+ 
+<div class="row foo" style="margin:30px auto 30px auto;">
+<div class="large-12 columns">
+		<div class="large-3 columns centerdiv">
+			<a href="#" class="button round blackblur fontslabo" >1</a>
+		</div>
+		<div class="large-3 columns centerdiv">
+			<a href="#" class="button round fontslabo" style="background-color:#2ecc71;">2</a>
+		</div>
+		<div class="large-3 columns centerdiv">
+			<a href="#" class="button round blackblur fontslabo">3</a>
+		</div>
+		<div class="large-3 columns centerdiv">
+			<a href="#" class="button round blackblur fontslabo">4</a>
+		</div>
+</div>
+
+</div>
+</div>
+ 
+<div class="row">
+	<div class="large-4 columns blackblur fontcolor" style="margin-left:-10px; padding:10px;">
+	
+		<div class="large-12 columns " >
+		<p><b>Check Date</b></p><hr>
+				<form action="checkavailability.php" method="post">
+				<div class="row">
